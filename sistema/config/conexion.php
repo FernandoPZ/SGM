57db55c4c276e36c6cf36efd79ad66abe68b1d8b
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2022-09-06 13:57:46 -0500

    Estructura principal

---
+++ sistema/config/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost'; //Guarda el nombre del host
+$user = 'root'; //Guarda el nombre del usuario
+$password = ''; //Guarda la contraseña
+$db = 'sgm'; //Guarda el nombre de la base de datos
+$conexion = @mysqli_connect($host,$user,$password,$db); //Realiza la conexion
+// --- Conexion con la bd ---
+// Ultima modificacion -- [06/09/2022 (13:55 hrs)]
+?>
---
+++ sistema/config/salir.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); // Inicia la sesion
+session_destroy(); // Destruye la sesion
+header('location: ../../index.php') // Redirecciona al login
+?>
+
+<!--
+--- Salir (Prototipo) ---
+Ultima modificacion -- [06/09/2022 (13:56 hrs)]
+-->
